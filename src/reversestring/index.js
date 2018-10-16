f140ca2813e5661ac73dee43d53861cadd28bd55
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-10-16 17:55:46 +0800

    reverse string solved

---
+++ src/reversestring/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+function reverse(str) {
+  let reversed = "";
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+
+module.exports = reverse;
